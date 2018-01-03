71d82a09da264084464603a7080443324209d91c
basinwang <wangbeishan@163.com>
2018-01-03 15:02:10 +0800

    fixed value binding

--- src/Select2.vue
+++ src/Select2.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <select class="form-control" :placeholder="placeholder" :value="innerValue" :disabled="disabled"></select>
+    <select class="form-control" :placeholder="placeholder" :disabled="disabled"></select>
   </div>
 </template>
 
 <script>
-import $ from 'jquery';
+import $ from "jquery";
 import "./select2/select2.js";
 
 export default {
-  name: 'Select2',
+  name: "Select2",
   data() {
     return {
-      innerValue: "",
       select2: null
     };
   },
-  model:{
-    event: 'change'
+  model: {
+    event: "change",
+    prop: "value"
   },
   props: {
     placeholder: {
@@ -34,41 +34,50 @@ export default {
     },
     value: null
   },
-  watch : {
-    options (val) {
-      if(val && val.length){
-        this.select2.select2({data: val});
-        const {id, text} = val[0];
-        this.$emit("change", id);
-        this.$emit("select", {id, text});
-        this.innerValue = text;        
-        this.select2.select2('val', [id]);
-        this.select2.trigger('change');
-      }
+  watch: {
+    options(val) {
+      this.setOption(val);
     },
     value(val) {
-      if(val){
-        this.innerValue = val;
-        this.select2.select2('val', [val]);
-        this.select2.trigger('change');
-      }
+      this.setValue(val);
     }
   },
-  mounted(){
-    this.select2 = $(this.$el).find('select').select2({
-            data: this.options,
-        }).on("select2:select", (ev) => {
-            const { id, text } = ev['params']['data'];
-            this.innerValue = text;
-            this.$emit("change", id);
-            this.$emit("select", {id, text});
-        })
+  methods: {
+    setOption(val) {
+      if (val && val.length) {
+        this.select2.select2({ data: val });
+        const { id, text } = val[0];
+        this.$emit("change", id);
+        this.$emit("select", { id, text });
+        this.select2.select2("val", [id]);
+        this.select2.trigger("change");
+      }
+    },
+    setValue(val) {
+      this.select2.select2("val", [val]);
+      this.select2.trigger("change");
+    }
   },
-  beforeDestroy(){
-    this.select2.select2('destroy');
+  mounted() {
+    this.select2 = $(this.$el)
+      .find("select")
+      .select2({
+        data: this.options
+      })
+      .on("select2:select", ev => {
+        const { id, text } = ev["params"]["data"];
+        this.$emit("change", id);
+        this.$emit("select", { id, text });
+      });
+    this.setOption(this.options);
+    if (this.value) {
+      this.setValue(this.value);
+    }
+  },
+  beforeDestroy() {
+    this.select2.select2("destroy");
   }
-}
-
+};
 </script>
 
 <style scope>
