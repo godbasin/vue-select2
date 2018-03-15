ab7bbe3562ac9e50a194e969b20feb77aae6e306
godbasin <wangbeishan@163.com>
2018-03-15 22:10:09 +0800

    fix mounded value

--- src/Select2.vue
+++ src/Select2.vue
@@ -5,24 +5,24 @@
 </template>
 
 <script>
-import $ from "jquery";
-import "./select2/select2.js";
+import $ from 'jquery';
+import './select2/select2.js';
 
 export default {
-  name: "Select2",
+  name: 'Select2',
   data() {
     return {
       select2: null
     };
   },
   model: {
-    event: "change",
-    prop: "value"
+    event: 'change',
+    prop: 'value'
   },
   props: {
     placeholder: {
       type: String,
-      default: ""
+      default: ''
     },
     options: {
       type: Array,
@@ -49,46 +49,44 @@ export default {
   methods: {
     setOption(val = []) {
       this.select2.empty();
-      this.select2.select2({ 
-         ...this.settings,
-         data: val 
+      this.select2.select2({
+        ...this.settings,
+        data: val
       });
-      this.select2.select2("val", []);
-      this.$emit("change", null);
-      this.select2.trigger("change");
+      this.select2.select2('val', []);
+      this.$emit('change', null);
+      this.select2.trigger('change');
     },
     setValue(val) {
       if (val instanceof Array) {
-        this.select2.select2("val", [...val]);
+        this.select2.select2('val', [...val]);
       } else {
-        this.select2.select2("val", [val]);
+        this.select2.select2('val', [val]);
       }
-      this.select2.trigger("change");
+      this.select2.trigger('change');
     }
   },
   mounted() {
     this.select2 = $(this.$el)
-      .find("select")
+      .find('select')
       .select2({
         ...this.settings,
         data: this.options
       })
-      .on("select2:select select2:unselect", ev => {
-        const { id, text, selected } = ev["params"]["data"];
+      .on('select2:select select2:unselect', ev => {
+        const { id, text, selected } = ev['params']['data'];
         const selectValue = this.select2.val();
-        this.$emit("change", selectValue);
-        this.$emit("select", { id, text, selected });
+        this.$emit('change', selectValue);
+        this.$emit('select', { id, text, selected });
       });
-    if (this.value) {
-      this.setValue(this.value);
-    }
+    this.setValue(this.value);
   },
   beforeDestroy() {
-    this.select2.select2("destroy");
+    this.select2.select2('destroy');
   }
 };
 </script>
 
 <style scope>
-@import "./select2/select2.min.css";
+@import './select2/select2.min.css';
 </style>
